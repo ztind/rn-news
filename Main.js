261ce8bb31825f848b8807060d43acaab5b73bcb
ztind <1356798719@qq.com>
2017-04-13 23:03:33 +0800

    Optimized android back return event

--- Main.js
+++ Main.js
@@ -3,7 +3,9 @@
  */
 import React,{Component} from 'react';
 import {
-    Navigator
+    Navigator,
+    BackAndroid,
+    ToastAndroid
     }
 from 'react-native';
 import SplashComponent from './view/SplashComponent';
@@ -15,6 +17,7 @@ class Main extends Component{
 
         return (
             <Navigator
+            	ref="refnavigator"
                 initialRoute={{name:defaultName,component:defaultComponent}}
 
                 configureScene={
@@ -34,5 +37,57 @@ class Main extends Component{
                 />
         );
     }
+    //***********************Android Back*************************
+
+    //组件安装完
+    componentDidMount(){
+        BackAndroid.addEventListener('androidBack', this._customAlertHandleBack.bind(this));
+    }
+    //组件卸载
+    componentWillUnmount(){
+        BackAndroid.removeEventListener('androidBack', this._customAlertHandleBack.bind(this));
+    }
+    _customAlertHandleBack(){
+        let navigator=this.refs.refnavigator;//父组件获取子组件对象用ref
+        if(navigator!==null){
+            let routes=navigator.getCurrentRoutes();//获取路由数组
+            if(routes.length>1){
+                //直接退出此层
+                navigator.pop();
+                return true;
+
+            }else{
+                //2s内按back退出
+                if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+                    return false;
+                }
+                this.lastBackPressed = Date.now();
+                ToastAndroid.show('再按一次退出应用',ToastAndroid.SHORT);
+                return true;
+            }
+        }
+    }
+    //1，自定义android back返回按钮事件
+    customAlertHandleBack = () =>{
+        //return true忽略默认的back退出，下次点击会继续回调该方法，false则使用默认back退出程序
+
+        Alert.alert('提示','确定要退出么?',
+            [
+                {text:'取消',onPress:() => {}},
+                {text:'确定',onPress:() => BackAndroid.exitApp()}
+            ]);
+        return true;
+    }
+
+    //2，2s内点击两次back退出
+    customClickTwoHandleBack = () => {
+        if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+            //最近2秒内按过back键，可以退出应用。
+            return false;
+        }
+        this.lastBackPressed = Date.now();
+        ToastAndroid.show('再按一次退出应用',ToastAndroid.SHORT);
+        return true;
+    };
 }
 export default Main;
